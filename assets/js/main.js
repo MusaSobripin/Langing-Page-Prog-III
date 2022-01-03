e4e41f06ef96da20976661685d24d0c545877002
Musa Sobripin <moisespinhao2001@gmail.com>
2022-01-03 12:01:28 -0300

    atualizando pag

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,48 @@ function botaoMenu(event){
 
 btnMobile.addEventListener('click', botaoMenu);
 btnMobile.addEventListener('touchstart', botaoMenu)
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+        setWrapperSize: true,
+      },
+    },
+  });
+
+  const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "70px",
+    duration: 700,
+    reset: true,
+  });
+  
+  scrollReveal.reveal(
+    ` 
+    #servicos .card, #sobre .image`,
+    { interval: 100 }
+  );
+  
+  
